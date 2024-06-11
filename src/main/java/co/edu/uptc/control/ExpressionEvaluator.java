fd6038f20f77be50023c8c8806cb5210b01a382f
Antr4ss <harold.guerrero02@uptc.edu.co>
2024-06-10 22:34:38 -0500

    Arreglo numeros negativos

--- src/main/java/co/edu/uptc/control/ExpressionEvaluator.java
+++ src/main/java/co/edu/uptc/control/ExpressionEvaluator.java
@@ -4,43 +4,43 @@ import java.util.Stack;
 
 public class ExpressionEvaluator extends CalculatorController {
 
-    public static double evaluate(String expression) {
-        char[] tokens = expression.toCharArray();
+public static double evaluate(String expression) {
+    char[] tokens = expression.toCharArray();
 
-        Stack<Double> values = new Stack<>();
-        Stack<Character> ops = new Stack<>();
+    Stack<Double> values = new Stack<>();
+    Stack<Character> ops = new Stack<>();
 
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i] == ' ')
-                continue;
+    for (int i = 0; i < tokens.length; i++) {
+        if (tokens[i] == ' ')
+            continue;
 
-            if ((tokens[i] >= '0' && tokens[i] <= '9') || (i == 0 && tokens[i] == '-')) {
-                StringBuffer buffer = new StringBuffer();
-                if (tokens[i] == '-') {
-                    buffer.append(tokens[i++]);
-                }
-                while (i < tokens.length && (tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.'))
-                    buffer.append(tokens[i++]);
-                values.push(Double.parseDouble(buffer.toString()));
-                i--;
-            } else if (tokens[i] == '(') {
-                ops.push(tokens[i]);
-            } else if (tokens[i] == ')') {
-                while (ops.peek() != '(')
-                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-                ops.pop();
-            } else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '^') {
-                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
-                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-                ops.push(tokens[i]);
+        if ((tokens[i] >= '0' && tokens[i] <= '9') || (tokens[i] == '-' && (i == 0 || tokens[i-1] == '('))) {
+            StringBuffer buffer = new StringBuffer();
+            if (tokens[i] == '-') {
+                buffer.append(tokens[i++]);
             }
+            while (i < tokens.length && (tokens[i] >= '0' && tokens[i] <= '9' || tokens[i] == '.'))
+                buffer.append(tokens[i++]);
+            values.push(Double.parseDouble(buffer.toString()));
+            i--;
+        } else if (tokens[i] == '(') {
+            ops.push(tokens[i]);
+        } else if (tokens[i] == ')') {
+            while (ops.peek() != '(')
+                values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+            ops.pop();
+        } else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '^') {
+            while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+            ops.push(tokens[i]);
         }
-
-        while (!ops.empty())
-            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-
-        return values.pop();
     }
+
+    while (!ops.empty())
+        values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+
+    return values.pop();
+}
 
     private static boolean hasPrecedence(char op1, char op2) {
         if (op2 == '(' || op2 == ')')
